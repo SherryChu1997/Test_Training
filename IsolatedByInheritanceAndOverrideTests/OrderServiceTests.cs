55dbb203aa94bb1a2a49fc713e1b48f218e3e5ac
sherry.chu@titansoft.com.sg <sherry.chu33@titansoft.com.sg>
2019-07-14 15:56:50 +0800

    要先了解狀況

--- IsolatedByInheritanceAndOverrideTests/OrderServiceTests.cs
+++ IsolatedByInheritanceAndOverrideTests/OrderServiceTests.cs
@@ -16,11 +16,16 @@ namespace IsolatedByInheritanceAndOverride.Tests
         public void Test_SyncBookOrders_3_Orders_Only_2_book_order()
         {
             //Try to isolate dependency to unit test
-
             //var target = new OrderService();
             //target.SyncBookOrders();
             //verify bookDao.Insert() twice
-            Assert.Fail();
+            //Assert.Fail();
+            //最終目標，他要能跑:)
+            //now:GetOrders()不work,  BookDao物件不能用(insert的方法)
+
+            //arrange:
+            //act:
+            //assert:
         }
     }
 }
